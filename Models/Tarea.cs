132909d451eae50b5d7627982edd8528ca940417
Juan David Gaviria Correa <63322751+JuanDa237@users.noreply.github.com>
2025-09-24 20:54:47 -0500

    build(tareas): crear modelo de tareas

---
+++ Models/Tarea.cs
@@ -0,0 +1,23 @@
+﻿namespace commITM2.Models
+{
+    /// <summary>
+    /// Representa una tarea.
+    /// </summary>
+    public class Tarea
+    {
+        /// <summary>
+        /// Identificador único de la tarea.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Descripción de la tarea.
+        /// </summary>
+        public string Descripcion { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Indica si la tarea está completada.
+        /// </summary>
+        public bool Completada { get; set; }
+    }
+}
